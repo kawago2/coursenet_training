aee6432cde91857e502238484b2cd87523b7643e
kawago <itkawago@gmail.com>
2023-09-13 19:33:43 +0700

    Clean Code

--- lib/sesi_tiga.dart
+++ lib/sesi_tiga.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MainPage extends StatefulWidget {
   const MainPage({
     super.key,
@@ -14,6 +13,10 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    );
   }
 }
